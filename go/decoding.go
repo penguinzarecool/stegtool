065673ff027ae36c9a0ab54d32090cc62106f092
penguinzarecool <151074650+penguinzarecool@users.noreply.github.com>
2025-04-24 17:07:21 -0700

    Add files via upload

---
+++ go/decoding.go
@@ -0,0 +1,39 @@
+package main
+
+func bitsToMessage(bits []byte) string {
+    msg := []byte{}
+    for i := 0; i < len(bits); i += 8 {
+        if i+8 > len(bits) {
+            break
+        }
+        var c byte = 0
+        for j := 0; j < 8; j++ {
+            c = (c << 1) | bits[i+j]
+        }
+        msg = append(msg, c)
+    }
+    return string(msg)
+}
+
+func DecodeStego(original, encoded []byte, offset int) string {
+    bits := []byte{}
+    max := len(original)
+    if len(encoded) < max {
+        max = len(encoded)
+    }
+
+    var endCtr int = 0
+    //for i := offset; i < max; i++ {
+    for i := offset; endCtr < 16; i++ {
+        bit := (original[i] ^ encoded[i])
+        if bit == 0 {
+            endCtr++
+        } else {
+            endCtr = 0
+        }
+        bits = append(bits, bit)
+    }
+
+    return bitsToMessage(bits)
+}
+
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module stegtool
+
+go 1.24.2
